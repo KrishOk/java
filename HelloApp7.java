14607fb2b2add19c67ad8775f97a6828173173be
KrishOk <106367587+KrishOk@users.noreply.github.com>
2026-03-26 10:36:37 +0530

    Add files via upload

---
+++ HelloApp7.java
@@ -0,0 +1,16 @@
+public class HelloApp7 {
+
+    public static void main(String[] args) {
+
+        
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+            return;
+        }
+
+
+        String names = String.join(", ", args);
+
+        System.out.println("Hello, " + names + "!");
+    }
+}
